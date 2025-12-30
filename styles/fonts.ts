48e17ddc32fcef9576a3f0a9733f91d44d89a714
Over12 <avalosfigueroapedro@gmail.com>
2025-12-29 20:50:10 -0500

    feat: Agregar estilos y fuentes

---
+++ styles/fonts.ts
@@ -0,0 +1,13 @@
+import { Rubik, Outfit } from 'next/font/google'
+
+export const rubik = Rubik({
+  subsets: ['latin'],
+  variable: '--font-rubik',
+  display: 'swap'
+})
+
+export const outfit = Outfit({
+  subsets: ['latin'],
+  variable: '--font-outfit',
+  display: 'swap'
+})
