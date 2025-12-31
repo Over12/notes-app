9634972254ce253b371e0fb4eab66e09390e35d1
Over12 <avalosfigueroapedro@gmail.com>
2025-12-31 17:38:30 -0500

    feat: Agregar ícono de exploración y mejorar etiquetas en la barra lateral

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,9 +1,9 @@
-import { IconNote, IconNotebook, IconSettings, IconUserCircle } from '@tabler/icons-react'
+import { IconNote, IconNotebook, IconPlanet, IconSettings, IconUserCircle } from '@tabler/icons-react'
 import Link from 'next/link'
 
 export default function Sidebar() {
   return (
-    <nav className='bg-text/10 p-5'>
+    <nav className='flex flex-col bg-text/5 p-5'>
       <div className='flex items-center justify-center px-5'>
         <IconNotebook className='text-accent size-12' />
         <p className='text-3xl font-semibold'>NoteMe</p>
@@ -18,16 +18,20 @@ export default function Sidebar() {
         <li className='rounded-lg bg-accent/25'>
           <Link href='/dashboard/notes' className='flex items-center px-3 py-2'>
             <IconNote className='size-7 text-accent' />
-            <span className='ml-2'>Notas</span>
+            <span className='ml-2'>Mis Notas</span>
           </Link>
         </li>
         <li className='rounded-lg bg-accent/25'>
-          <Link href='/dashboard/settings' className='flex items-center px-3 py-2'>
-            <IconSettings className='size-7 text-accent' />
-            <span className='ml-2'>Configuración</span>
+          <Link href='/dashboard/explore' className='flex items-center px-3 py-2'>
+            <IconPlanet className='size-7 text-accent' />
+            <span className='ml-2'>Muro</span>
           </Link>
         </li>
       </ul>
+      <Link href='/dashboard/settings' className='flex items-center px-3 py-2 mt-auto rounded-lg bg-accent/25'>
+        <IconSettings className='size-7 text-accent' />
+        <span className='ml-2'>Configuración</span>
+      </Link>
     </nav>
   )
 };
