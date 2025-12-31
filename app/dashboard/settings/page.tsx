b9aadf48c3b6a2ba127b477b9bd699a2bf5b23ac
Over12 <avalosfigueroapedro@gmail.com>
2025-12-31 17:38:48 -0500

    feat: Eliminar fuente Rubik de los encabezados en la página de configuración

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -5,12 +5,12 @@ import StyleSection from '@/components/common/StyleSection'
 export default function Page() {
   return (
     <main className='p-10'>
-      <h2 className='font-rubik text-2xl font-bold mb-2'>Personalización</h2>
+      <h2 className='text-2xl font-bold mb-2'>Personalización</h2>
       <div className='flex gap-5'>
         <StyleSection />
         <ModeSection />
       </div>
-      <h2 className='font-rubik text-2xl font-bold mt-7 mb-2'>Datos</h2>
+      <h2 className='text-2xl font-bold mt-7 mb-2'>Datos</h2>
       <DeleteDataButton />
     </main>
   )
